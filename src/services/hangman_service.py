e404fae75822430df8e29d95fc3e8bc9057def15
felipe.simoes <felipe.simoes@ambientalserra.com.br>
2024-04-16 09:23:05 -0300

    Atualização da listagem de palavras

--- src/services/hangman_service.py
+++ src/services/hangman_service.py
@@ -1,46 +1,143 @@
 import random, string, requests, re, os
 from dotenv import load_dotenv
 
+
 class HangmanService:
 
     def __init__(self):
-        
+
         load_dotenv()
-        
+
         self.lista_palavras = [
-            "QUEEN",
-            "KISS",
-            "BLIND-GUARDIAN",
-            "HAMMERFALL",
-            "HELLOWEEN",
-            "IRON-MAIDEN",
-            "METALLICA",
-            "NIGHTWISH",
-            "DRAGONFORCE",
-            "MANOWAR",
-            "MEGADETH",
-            "DIO",
+            "ABORTED",
             "ACCEPT",
-            "EPICA",
-            "SONATA-ARCTICA",
-            "SABATON",
-            "JUDAS-PRIEST",
+            "AEROSMITH",
             "AMON-AMARTH",
-            "STRATOVARIUS",
-            "GRAVE-DIGGER",
-            "SONIC-SYNDICATE",
-            "ICED-EARTH",
-            "FREEDOM-CALL",
-            "RHAPSODY",
+            "ANCIENT",
+            "ANTHRAX",
+            "ANVIL",
+            "ASPHYX",
+            "AT-THE-GATES",
+            "AUTOPSY",
+            "BATHORY",
+            "BATUSHKA",
+            "BEHEMOTH",
+            "BEHEXEN",
+            "BELPHEGOR",
+            "BELZEBUBS",
+            "BENEDICTION",
+            "BLACK-SABBATH",
             "BLIND-GUARDIAN",
-            "HAMMERFALL",
-            "HELLOWEEN",
-            "IRON-MAIDEN",
-            "METALLICA",
-            "NIGHTWISH",
+            "CANDLEMASS",
+            "CANNIBAL-CORPSE",
+            "CARCASS",
+            "CARPATHIAN-FOREST",
+            "CELTIC-FROST",
+            "CRAFT",
+            "CREEDENCE-CLEARWATER-REVIVAL",
+            "CRIMSON-GLORY",
+            "CRYPTOPSY",
+            "DARK-FUNERAL",
+            "DARK-TRANQUILLITY",
+            "DARKTHRONE",
+            "DEATH",
+            "DECAPITATED",
+            "DEEP-PURPLE",
+            "DEICIDE",
+            "DIAMOND-HEAD",
+            "DIMMU-BORGIR",
+            "DIO",
+            "DISMEMBER",
+            "DISSECTION",
             "DRAGONFORCE",
+            "EMPEROR",
+            "ENSLAVED",
+            "ENTHRONED",
+            "ENTOMBED",
+            "EPICA",
+            "EXODUS",
+            "FREEDOM-CALL",
+            "GORGOROTH",
+            "GRAVE",
+            "GRAVE-DIGGER",
+            "HAMMERFALL",
+            "HATE-ETERNAL",
+            "HELLOWEEN",
+            "HYPOCRISY",
+            "ICED-EARTH",
+            "IMMOLATION",
+            "IMMORTAL",
+            "INCANTATION",
+            "IRON-MAIDEN",
+            "JIMI-HENDRIX",
+            "JUDAS-PRIEST",
+            "KATAKLYSM",
+            "KING-DIAMOND",
+            "KISS",
+            "KREATOR",
+            "KRISIUN",
+            "LED-ZEPPELIN",
+            "LYNYRD-SKYNYRD",
+            "MALEVOLENT-CREATION",
             "MANOWAR",
+            "MARDUK",
+            "MASSACRE",
+            "MAYHEM",
+            "MEGADETH",
+            "MERCYFUL-FATE",
+            "METALLICA",
+            "MORBID-ANGEL",
+            "NAPALM-DEATH",
+            "NARGAROTH",
+            "NECROPHAGIST",
+            "NECROPHOBIC",
+            "NIGHTWISH",
+            "NILE",
+            "OBITUARY",
+            "OVERKILL",
+            "PANTERA",
+            "PESTILENCE",
+            "PINK-FLOYD",
+            "POSSESSED",
+            "PUNGENT-STENCH",
+            "QUEEN",
+            "RHAPSODY",
+            "ROTTING-CHRIST",
+            "RUNNING-WILD",
+            "SABATON",
+            "SAMAEL",
+            "SARGEIST",
+            "SATYRICON",
+            "SAVATAGE",
+            "SAXON",
+            "SCORPIONS",
+            "SEPULTURA",
+            "SHINING",
+            "SIX-FEET-UNDER",
+            "SLAYER",
+            "SONATA-ARCTICA",
+            "SONIC-SYNDICATE",
+            "STORMWITCH",
+            "STRATOVARIUS",
+            "SUFFOCATION",
+            "SUMMONING",
+            "TAAKE",
+            "TERRORIZER",
+            "TESTAMENT",
+            "THE-BEATLES",
+            "THE-DOORS",
+            "THE-ROLLING-STONES",
+            "THE-WHO",
+            "TSJUDER",
+            "UNLEASHED",
+            "VADER",
+            "VAN-HALEN",
+            "VENOM",
+            "VITAL-REMAINS",
+            "VOIVOD",
+            "WATAIN",
         ]
+
         self.tentativas_erradas = []
         self.tentativas_corretas = []
         self.palavra_aleatoria = self.obter_palavra_aleatoria()
@@ -86,15 +183,15 @@ class HangmanService:
         url_info = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist={}&api_key={}&format=json".format(
             nome_artista, self.api_key
         )
-        
+
         url_top_albums = "http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist={}&api_key={}&format=json".format(
             nome_artista, self.api_key
         )
-        
+
         url_top_tracks = "http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist={}&api_key={}&format=json".format(
             nome_artista, self.api_key
         )
-        
+
         print(url_top_albums)
         response_info = requests.get(url_info)
         response_top_albums = requests.get(url_top_albums)
@@ -114,30 +211,38 @@ class HangmanService:
                 if re.search(r"(\d{4})", data_info["artist"]["bio"]["summary"])
                 else None
             )
-            
+
             formed_year_content = (
                 re.search(r"(\d{4})", data_info["artist"]["bio"]["content"]).group(1)
                 if re.search(r"(\d{4})", data_info["artist"]["bio"]["content"])
                 else None
             )
-            
-            ano_formacao = formed_year_summary if formed_year_summary is not None else formed_year_content
-            
+
+            ano_formacao = (
+                formed_year_summary
+                if formed_year_summary is not None
+                else formed_year_content
+            )
+
             top_albums = data_top_albums["topalbums"]["album"][:3]
             top_albums_list = []
 
             for album in top_albums:
                 album_name = album["name"]
-                album_large_image = [img["#text"] for img in album["image"] if img["size"] == "large"][0]
-                top_albums_list.append({"name": album_name, "large_image": album_large_image})
-            
+                album_large_image = [
+                    img["#text"] for img in album["image"] if img["size"] == "large"
+                ][0]
+                top_albums_list.append(
+                    {"name": album_name, "large_image": album_large_image}
+                )
+
             top_tracks = data_top_tracks["toptracks"]["track"][:5]
             top_tracks_list = []
-            
+
             for track in top_tracks:
                 track_name = track["name"]
                 top_tracks_list.append(track_name)
-            
-            return ano_formacao, top_albums_list, top_tracks_list  
+
+            return ano_formacao, top_albums_list, top_tracks_list
         else:
             print("Erro ao acessar a API:", response_info.status_code)
